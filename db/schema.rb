21f64d818d2fa5986f80f25425de56a0e7c11d07
shanmukh49 <shanmukha49reddy@gmail.com>
2013-11-30 11:09:02 +0530

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131129042842) do
+ActiveRecord::Schema.define(:version => 20131130052903) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(:version => 20131129042842) do
   end
 
   create_table "questions", :force => true do |t|
-    t.string   "text"
+    t.text     "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "subject_id"
