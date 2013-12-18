94579a3329e6585b85568ed7b02bcaae92659ae1
shanmukh49 <shanmukha49reddy@gmail.com>
2013-12-18 15:39:28 +0530

    update avatar

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,15 @@
 
 class Question < ActiveRecord::Base
-  attr_accessible :text,:subject_id,:avatar
+  attr_accessible :text,:subject_id,:avatar,:avatar_file_name, :avatar_content_type, :avatar_file_size,:avatar_updated_at
   has_many :choices
   has_many :result, :through=>:user_answers
   has_one :user_answer
   belongs_to :subject
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
   
   #has_many :users, :through => :types
   #set_primary_key 'type'
